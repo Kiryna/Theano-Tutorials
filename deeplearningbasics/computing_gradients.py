a2aadb5d251b38bb85041789a18381ae7ccd0c00
Iryna Kharaborkina <i.kharaborkina@gmail.com>
2015-04-28 14:07:49 +0300

    several variables function

--- deeplearningbasics/computing_gradients.py
+++ deeplearningbasics/computing_gradients.py
@@ -3,15 +3,16 @@ from theano import pp
 from theano import function
 
 x = T.dscalar('x')
-y = (x + 5) / (x ** 2 + 1)
-gy = T.grad(y, x)
-print pp(gy)
+y = T.dscalar('y')
+z = T.dscalar('y')
+mult_var_func = x ** 3 + y ** 2 + z
+grad = T.grad(mult_var_func, [x, y, z])  # [3 * x ** 2, 2 * y, 1]
 
-f = function([x], gy)
-print f(4)
-print f(94.2)
+f = function([x, y, z], grad)
+print f(1, 2, 3)
+print f(1, 1, 1)
 
-print pp(f.maker.fgraph.outputs[0])
+print '[', pp(f.maker.fgraph.outputs[0]), ', ', pp(f.maker.fgraph.outputs[1]), ', ', pp(f.maker.fgraph.outputs[2]), ']'
 
 a = T.dmatrix('a')
 s = T.sum(1 / (1 + T.exp(-a)))
